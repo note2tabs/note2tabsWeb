1ae97191e0a49bc50b15f53c8d3b0e962231b2d4
selon14 <Noel.solomon1406@gmail.com>
2026-02-09 22:00:35 +0100

    Remove env.txt and ignore it

--- lib/stripe.ts
+++ lib/stripe.ts
@@ -2,8 +2,4 @@ import Stripe from "stripe";
 
 const secretKey = process.env.STRIPE_SECRET_KEY;
 
-export const stripeClient = secretKey
-  ? new Stripe(secretKey, {
-      apiVersion: "2023-10-16",
-    })
-  : null;
+export const stripeClient = secretKey ? new Stripe(secretKey) : null;
